2a0ca5ed5d3efd16caea9ba799a09571a8c32bba
Marshall Oliver <olivermt91@gmail.com>
2020-05-25 14:24:20 -0400

    Seeder updates to include default admin
    Included Super User seeds and edits to the .env file to define the default admin.  Included additional vars in .env.example to seed admin information.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
 
     	$this->call(RolesSeeder::class);
-        $this->call(usersSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(RoleUsersSeeder::class);
     }
 }
