6383e3a69ff6c3c0143fbf351cc1c06d17a27621
Marshall Oliver <olivermt91@gmail.com>
2020-06-18 09:50:43 -0400

    Add ApplicationInfo Controller, Model, and Resource

---
+++ app/Http/Controllers/CenterEdge/ApplicationInfoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\CenterEdge;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApplicationInfoController extends Controller
+{
+    //
+}
